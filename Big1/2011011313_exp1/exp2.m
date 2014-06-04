a19e4b25d06b3c2f9ca5ae8d35828cf3d6da0a16
Zhe Shen <szsxt818@gmail.com>
2014-06-04 17:47:30 +0800

    BIG 1 code finished

--- Big1/2011011313_exp1/exp2.m
+++ Big1/2011011313_exp1/exp2.m
@@ -5,12 +5,12 @@ clear all;
 %Transfer To Gray-Scale
 IMG_original = imread('lena.bmp');
 IMG_original = imresize(IMG_original, [512, 512])
-subplot(3, 2, 1);
+subplot(2, 2, 1);
 %size(IMG_original)
 imshow(IMG_original);
 title('Original Image');
 IMG_Gray = rgb2gray(IMG_original);
-subplot(3, 2, 2);
+subplot(2, 2, 2);
 imshow(IMG_Gray);
 title('Gray Image');
 imwrite(IMG_Gray, 'F:\Programming\多媒体\hw\Big1\2011011313_exp1\res_pics\exp2\lenaGray.bmp');
@@ -25,7 +25,7 @@ Q = [16 11 10 16 24 40 51 61
      49 64 78 87 103 121 120 101
      72 92 95 98 112 100 103 99];
  
- Canon = [1 1 1 2 3 6 8 10 
+ Cannon = [1 1 1 2 3 6 8 10 
           1 1 2 3 4 8 9 8 
           2 2 2 3 6 8 10 8 
           2 2 3 4 7 12 11 9 
@@ -62,22 +62,87 @@ end
 encode_2D_blocks_Q = cell2mat(temp1);
 
 %Applying 2D IDCT and calculate the PSNR
+sum1 = 0;
 for i = 1 : 1 : 512/8
     for j = 1 : 1 : 512/8
         temp2{i, j} = idct2(temp1{i, j});
         psnr(i, j) = PSNR(blocks{i, j}, temp2{i, j});
+        sum1 = sum1 + psnr(i, j);
     end
 end 
-disp(psnr);
+save('F:\Programming\多媒体\hw\Big1\2011011313_exp1\exp2_data_res\Q_block_psnr.txt', 'psnr', '-ascii', '-tabs');
 
 %Calculate the average PSNR
 decode_2D_blocks_Q = cell2mat(temp2);
 decode_2D_blocks_Q_img = mat2gray(decode_2D_blocks_Q);
-psnr1 = PSNR(gray, decode_2D_blocks_Q);
-disp(psnr1);
-subplot(3, 2, 3);
+psnr_avg = sum1 / (64 * 64);
+disp(psnr_avg);
+subplot(2, 2, 3);
 imshow(decode_2D_blocks_Q_img);
 title('decode 2D blocks Q img');
 imwrite(decode_2D_blocks_Q_img, 'F:\Programming\多媒体\hw\Big1\2011011313_exp1\res_pics\exp2\decode_2D_blocks_Q.bmp');
 
 %Use a*Q as quantization matrix
+cnt = 1;
+for a = 0.1 : 0.1 : 2
+    %quantize the DCT coefficients
+    for i = 1 : 1 : 512/8
+        for j = 1 : 1 : 512/8
+            temp3{i, j} = round(temp{i, j}./(a*Q)).*(a*Q);
+        end
+    end
+    %Applying 2D IDCT and calculate the PSNR
+    for i = 1 : 1 : 512/8
+        for j = 1 : 1 : 512/8
+            temp4{i, j} = idct2(temp3{i, j});
+            psnr1(i, j) = PSNR(blocks{i, j}, temp4{i, j});
+        end
+    end
+    filename1 = strcat('F:\Programming\多媒体\hw\Big1\2011011313_exp1\exp2_data_res\Q_block_psnr', num2str(a));
+    filename1_txt = strcat(filename1, '.txt');
+    save(filename1_txt, 'psnr1', '-ascii', '-tabs');
+    cur_mat = cell2mat(temp4);
+    psnr_arr(cnt) = PSNR(gray, cur_mat);
+    filename2 = strcat('F:\Programming\多媒体\hw\Big1\2011011313_exp1\res_pics\exp2\as\Q_block_psnr', num2str(a));
+    filename_img = strcat(filename2, '.bmp');
+    imwrite(mat2gray(cur_mat), filename_img);
+    cnt = cnt + 1;
+end
+%plot the curve of a and PSNR
+figure()
+a = 0.1 : 0.1 : 2
+plot(a, psnr_arr);
+
+%cannon Q
+sum2 = 0;
+for i = 1 : 1 : 512/8
+    for j = 1 : 1 : 512/8
+        temp5{i, j} = round(temp{i, j}./Cannon).*Cannon;
+        temp6{i, j} = idct2(temp5{i, j});
+        psnr_cannon(i, j) = PSNR(blocks{i, j}, temp6{i, j});
+        sum2 = sum2 + psnr_cannon(i, j);
+    end
+end
+Cannon_blocks = cell2mat(temp6);
+psnr_cannon_aver = sum2 / (64 * 64);
+disp(psnr_cannon_aver);
+save('F:\Programming\多媒体\hw\Big1\2011011313_exp1\exp2_data_res\Q_block_psnr_cannon.txt', 'psnr_cannon', '-ascii', '-tabs');
+Cannon_blocks_img = mat2gray(Cannon_blocks);
+imwrite(Cannon_blocks_img, 'F:\Programming\多媒体\hw\Big1\2011011313_exp1\res_pics\exp2\Cannon_blocks_img.bmp');
+
+%Nikon Q
+sum3 = 0;
+for i = 1 : 1 : 512/8
+    for j = 1 : 1 : 512/8
+        temp7{i, j} = round(temp{i, j}./Nikon).*Nikon;
+        temp8{i, j} = idct2(temp7{i, j});
+        psnr_nikon(i, j) = PSNR(blocks{i, j}, temp8{i, j});
+        sum3 = sum3 + psnr_nikon(i, j);
+    end
+end
+Nikon_blocks = cell2mat(temp8);
+psnr_Nikon_aver = sum3 / (64 * 64);
+disp(psnr_Nikon_aver);
+save('F:\Programming\多媒体\hw\Big1\2011011313_exp1\exp2_data_res\Q_block_psnr_nikon.txt', 'psnr_nikon', '-ascii', '-tabs');
+Nikon_blocks_img = mat2gray(Nikon_blocks);
+imwrite(Nikon_blocks_img, 'F:\Programming\多媒体\hw\Big1\2011011313_exp1\res_pics\exp2\Nikon_blocks_img.bmp');
